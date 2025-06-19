2695fbfa2a6c254cdafa18a6a7ef23f89ebe884b
Learnerbypassion <callmedad002022@gmail.com>
2025-06-19 20:37:20 +0530

    Update transposeToitsown.c
    Okay, so there was an issue or bug whatever. Actually  I was printing arr[j][i] which gives me the correct transpose, but the code does not change the main memory of the arr[i][j], because for the wrong loop condition, the loop changes the matrix twice, so it becomes same as it is, so now I have fixed this.
    
    BTW why are you reading this ? Don't you have any work?

--- transposeToitsown.c
+++ transposeToitsown.c
@@ -18,7 +18,7 @@ int main(){
         printf("\n");
     }
     for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+        for(int j=i+1;j<n;j++){
             int temp = arr[i][j];
             arr[i][j] = arr[j][i];
             arr[j][i] = temp;
@@ -27,7 +27,7 @@ int main(){
     printf("The transpose is: \n");
     for(int i=0 ; i<n; i++){
         for(int j=0 ; j<n; j++){
-            printf("%d ",arr[j][i]);
+            printf("%d ",arr[i][j]);
         }
         printf("\n");
     }
